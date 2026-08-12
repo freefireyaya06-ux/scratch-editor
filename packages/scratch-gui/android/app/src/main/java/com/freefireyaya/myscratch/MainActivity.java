e7c9c3e87e4a1ee428973f280a3daceab4d5859e
freefireyaya06-ux <freefireyaya06@gmail.com>
2026-08-12 07:45:59 +0000

    feat: add capacitor android project

---
+++ packages/scratch-gui/android/app/src/main/java/com/freefireyaya/myscratch/MainActivity.java
@@ -0,0 +1,5 @@
+package com.freefireyaya.myscratch;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ packages/scratch-gui/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.freefireyaya.myscratch',
+  appName: 'MyScratch',
+  webDir: 'build'
+};
+
+export default config;
